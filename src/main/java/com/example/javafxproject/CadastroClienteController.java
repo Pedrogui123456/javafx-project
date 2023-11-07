3003b6afd7dc6d1cfb1f49d369a143cf0c4170da
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2023-11-07 15:15:09 -0300

    javafx-project

---
+++ src/main/java/com/example/javafxproject/CadastroClienteController.java
@@ -0,0 +1,26 @@
+package com.example.javafxproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CadastroClienteController {
+    @FXML
+    private TextField txfNome;
+    @FXML
+    private TextField txfEmail;
+    @FXML
+    private TextField txfSenha;
+
+    public void onActionCadastrar() {
+        String nome = txfNome.getText();
+        String email = txfEmail.getText();
+        String senha = txfSenha.getText();
+
+
+        System.out.println(nome);
+        System.out.println(email);
+        System.out.println(senha);
+ 
+    }
+    
+}
